633871f0490cda6e96098cfb52eae193fe7dc947
MarcilioeKleyton <marcilioav46@gmail.com>
2025-07-23 17:31:16 +0000

    fim

--- tests/caculadora.test.js
+++ tests/caculadora.test.js
@@ -1,7 +1,13 @@
 // @ts-check
 
+const calculadora = require("../models/calculadra")
 
 
 test("Criando primeiro teste", function(){
     console.log("oie");
 })
+test ("Soma de 2 + 2 deve retornar 4", function(){
+const resultado = calculadora.soma(2,2)
+expect(resultado).toBe(4)
+})
+test("soma de 2 + 4")
