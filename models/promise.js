a2e2ab4881b7bec2245d76619afd71018b74dae9
MarcilioeKleyton <marcilioav46@gmail.com>
2025-07-31 17:25:27 +0000

    ok

--- models/promise.js
+++ models/promise.js
@@ -1,9 +1,24 @@
-const readFile = require("node:readilene");
-const terminal = readilene.createInterface({
+const readFile = require("node:readline");
+const terminal = readFile.createInterface({
     input: process. stdin,
     output: process.stdout,
 });
 
-terminal.question("Qual seu nome: \n", function (valor) {
-    console.log("seja bem vindo "+ valor);
+// terminal.question("Qual seu nome: \n", function (valor) {
+//     console.log("seja bem vindo "+ valor);
+//     terminal.close();
+// });
+
+function question(pergunta) {
+ return new Promise(function (resolve, reject) {
+    terminal.question(pergunta + "\n", function (valor) {
+        resolve(valor)
+    });
+  });
+}
+question("Qual seu nome").then(function (valor) {
+    console.log(`Seja bem vindo $ {valor}`);
+});
+.finally(function () {
+    terminal.close();
 });
