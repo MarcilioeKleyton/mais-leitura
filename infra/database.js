a3dce7a1b9f8bda228857998a3123a7ef95294d0
MarcilioeKleyton <marcilioav46@gmail.com>
2025-09-25 14:31:19 +0000

    fim

---
+++ infra/database.js
@@ -0,0 +1,18 @@
+const { Client } = require("pg");
+
+async function query(sql, args = []) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "marcilio",
+    database:"maisleitura",
+    password: "1234",
+  });
+  await client.connect();
+
+  const res = await client.query(sql, args);
+
+  await client.end();
+  return res.rows;
+}
+module.exports = { query };
