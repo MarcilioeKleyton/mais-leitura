50b9470ff549069df4d18c538401c2fa01f2a0f6
MarcilioeKleyton <marcilioav46@gmail.com>
2025-11-05 16:47:52 +0000

    ok

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const livroRepository = require("./infra/repository/livro");
+const http = require("node:http");
 
-livroRepository
-.consultarTodos()
-.then(function (resultado) {
-    console.log(resultado);
-})
-.catch(function (erro) {
-    console.log("Deu ruim", erro);
+const server = http.createServer(function (req, res) {
+  //res.writeHead(200, {"content-type": "text/plain"})
+  res.end("Ok bem vindo");
 });
 
+const port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log("Inicializando servidor HTTP na porta " + port);
+});
