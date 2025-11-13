f9c03eab94a5dbd506d57e5e4ff20d8148b73316
MarcilioeKleyton <marcilioav46@gmail.com>
2025-11-13 17:28:40 +0000

    ok

--- infra/repository/livro.js
+++ infra/repository/livro.js
@@ -4,10 +4,10 @@ const sql = `SELECT * FROM livro`;
 const resultados = await query(sql)
 return resultados;
 }
-async function consultarTops() {
+async function consultaTops() {
 const sql = `SELECT * FROM livro WHERE top_livro = true`;
 const resultados = await query(sql);
 return resultados;
 }
 
-module.exports = { consultarTodos, consultarTops };
+module.exports = { consultarTodos, consultaTops };
